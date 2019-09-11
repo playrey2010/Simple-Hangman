7cca52616d14ad68d927900d84003b3a41c79b25
playrey2010 <playrey2010@gmail.com>
2019-09-11 11:21:23 -0400

    finished...

--- src/SimpleHangman.java
+++ src/SimpleHangman.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -5,67 +6,90 @@ public class SimpleHangman {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random r = new Random();
-        String word = "";
-        String answer = "";
         char guess = 'a';
+        String word = "";
+        String tempAnswer = "";
         String[] answers = {"tree", "rain", "bear", "encourage", "promise",
-        "soup", "chess", "insurance", "pancakes", "stream"};
-        int lifeCounter = 5;
+                "soup", "chess", "insurance", "pancakes", "stream"};
+        int lives = 5;
+        boolean change = false;
+        boolean victory = false;
 
-
-        // Choose a word randomly
+        // Choose word randomly
+        // word == 1 of answers
         word = answers[r.nextInt(10)];
-        System.out.println(word);
-        // new variable with just underscores, same length
-        answer = word.replaceAll("[a-zA-Z]", "_");
+
         // create char array containing answer word
-        char [] word1 = new char[word.length()];
+        char [] wordArray = new char[word.length()];
         for (int i =0 ; i < word.length(); i++) {
-            word1[i] = word.charAt(i);
+            wordArray[i] = word.charAt(i);
         }
 
-        char [] answer1 = new char[answer.length()];
+        // Create temporary answer with underscores
+        tempAnswer = word.replaceAll("[a-zA-Z]", "_");
+
+        // create char array containing underscores
+        char [] answerNow = new char[tempAnswer.length()];
         for (int i =0 ; i < word.length(); i++) {
-            answer1[i] = answer.charAt(i);
+            answerNow[i] = tempAnswer.charAt(i);
         }
-        while (lifeCounter > 0) {
-            System.out.print("\nYour guess: ");
-            guess = sc.next().charAt(0);
-            for (int i = 0; i < answer1.length; i++) {
-                int rightGuess = 0;
-                if (guess == word1[i]) {
-                    answer1[i] = guess;
-                    rightGuess++;
+
+        for (char i : answerNow) {
+            System.out.print(i + " ");
+        }
+
+
+        while (lives>0) {
+            // long way to compare would include a nested for loop going through both array indexes and setting multiple conditions
+            if (Arrays.equals(answerNow, wordArray)) {
+                victory = true;
+                break;
+            } else {
+                System.out.print("\nYour guess: ");
+                guess = sc.next().charAt(0);
+                change = false;
+                for (int i =0; i < answerNow.length; i++) {
+                    if (guess == wordArray[i]) {
+                        answerNow[i] = guess;
+                        change = true;
+                    }
                 }
-//                System.out.print(answer1[i]);
-            }
 
-            for (int i =0 ; i < word.length(); i++) {
-                System.out.print(answer1[i]);
+                if (change == true) {
+                    System.out.print("Your guess so far: ");
+                    for (char i : answerNow) {
+                        System.out.print(i + " ");
+                    }
+                } else {
+                    lives--;
+                    switch (lives) {
+                        case 4:
+                        case 3:
+                        case 2:
+                            System.out.println("You still have " + lives + " chances!");
+                            break;
+                        case 1:
+                            System.out.println("You have one more chance!");
+                            break;
+                    }
+                    System.out.print("Your guess so far: ");
+                    for (char i : answerNow) {
+                        System.out.print(i + " ");
+                    }
+                }
+
             }
 
         }
-
-        for (int i =0 ; i < word.length(); i++) {
-            System.out.print(answer1[i]);
+        if (victory) {
+            System.out.println("\nYou win!!");
         }
-
-
-//        System.out.println(word);
-//        char [] answer1 = new char[word.length()];
-//        for (int i =0 ; i < word.length(); i++) {
-//            answer1[i] = word.charAt(i);
-//            System.out.println(answer1[i]);
-//        }
-
-
-
-
-
-
-// is there a way to use for letter:word and make it work?
-//        for (String letter:word) {
-//            System.out.println(letter);
-//        }
+        else {
+            System.out.println("\nBetter luck next time!");
+            System.out.print("The answer was ");
+            for (char i : wordArray) {
+                System.out.print(i + " ");
+            }
+        }
     }
 }
